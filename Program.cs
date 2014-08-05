6aeacca3423278ea31aaa7e918fe96f18debc103
Eric Schles <ericschles@gmail.com>
2014-08-05 09:31:13 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car;
+
+            car = new Car("Blue");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+
+            Console.WriteLine(car.DoStuff());
+            Console.ReadKey();
+        }
+    }
+
+    struct Car
+    {
+        private string color;
+
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+        public int DoStuff()
+        {
+            return 5 + 7;
+        }
+        public string Describe()
+        {
+            return "This car is " + color;
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+}
